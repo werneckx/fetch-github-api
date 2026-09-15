216c2496ba8f8dacbbf20423916b4091c39e13d2
Edson Rodrigues <er4273270@gmail.com>
2026-09-15 13:29:18 -0300

    feat(scripts) keyup input search

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,23 @@
-async function search(username) {
-    const response = await fetch(`https://api.github.com/users/${username}`)
-    return await response.json()
-}
-
 document.getElementById('btn-search').addEventListener('click', () => {
     const username = document.getElementById('input-search').value
     getUserProfile(username)
 })
+
+document.getElementById('input-search').addEventListener('keyup', (e) =>{
+    const username = e.target.value
+    const key = e.which || e.keyCode
+    const isEnterKeyPressed = key === 13
+
+    if (isEnterKeyPressed){
+        getUserProfile(username)
+    }
+})
+
+
+async function search(username) {
+    const response = await fetch(`https://api.github.com/users/${username}`)
+    return await response.json()
+}
 
 function getUserProfile(username) {
     search(username).then(
