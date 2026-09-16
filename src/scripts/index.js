80250fe1aa2ea982e80df671ef046b3922d7fe96
Edson Rodrigues <er4273270@gmail.com>
2026-09-16 13:37:02 -0300

    faet(scripts) separate responsibilities and add variables

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,3 +1,7 @@
+
+import { user } from "./services/user.js"
+import { repositories } from "./services/repositories.js"
+
 document.getElementById('btn-search').addEventListener('click', () => {
     const userName = document.getElementById('input-search').value
     getUserProfile(userName)
@@ -15,19 +19,8 @@ document.getElementById('input-search').addEventListener('keyup', (e) => {
     }
 })
 
-
-async function search(userName) {
-    const response = await fetch(`https://api.github.com/users/${userName}`)
-    return await response.json()
-}
-
-async function repos(userName) {
-    const response = await fetch(`https://api.github.com/users/${userName}/repos`)
-    return await response.json()
-}
-
 function getUserProfile(userName) {
-    search(userName).then(
+    user(userName).then(
         userData => {
             let userInfo =
                 `
@@ -46,7 +39,7 @@ function getUserProfile(userName) {
 }
 
 function getUserRepositories(userName) {
-    repos(userName).then(
+    repositories(userName).then(
         reposData => {
             let repositoriesItens = ""
 
